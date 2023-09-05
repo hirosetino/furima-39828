f9d1a9bbb132d0b16eb8a528aff79bc460c9e1df
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-05 18:38:24 +0900

    バリデーションの再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
+  validates :nickname, presence: true
+
+  VALID_LAST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  validates :last_name, presence: true, format: { with: VALID_LAST_NAME_REGEX }
+
+  VALID_FIRST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  validates :first_name, presence: true, format: { with: VALID_FIRST_NAME_REGEX }
+
+
+  VALID_LAST_NAME_KANA_REGEX = /\A[ァ-ヶ一]+\z/.freeze
+  validates :last_name_kana, presence: true, format: { with: VALID_LAST_NAME_KANA_REGEX }
+
+
+  VALID_FIRST_NAME_KANA_REGEX = /\A[ァ-ヶ一]+\z/.freeze
+  validates :first_name_kana, presence: true, format: { with: VALID_FIRST_NAME_KANA_REGEX }
+
+
+  validates :birth_date, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
