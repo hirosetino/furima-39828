0f787d59bb2e2cbc672f59471ebd7467f2085b42
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-06 15:35:25 +0900

    Active Storage導入２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,10 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :content, :category_id, :state_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :price, :image)
+  end
 end
