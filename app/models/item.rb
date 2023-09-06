10b4ff66c539e8a0b5c80569c5c7127df211e0b5
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-06 16:01:29 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :state
+    belongs_to :shipping_cost
+    belongs_to :prefecture
+    belongs_to :days_to_ship
+
     has_one_attached :image
+
+
+    validates :category_id, numericality: { other_than: 1 }
+    validates :state_id, numericality: { other_than: 1 }
+    validates :shipping_cost_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :days_to_ship_id, numericality: { other_than: 1 }
 end
