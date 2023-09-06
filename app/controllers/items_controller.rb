7f32ad61798d8ab1a06d0cae35f1e34d5e6fd171
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-06 17:05:05 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :content, :category_id, :state_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :price, :image)
+    params.require(:item).permit(:name, :content, :category_id, :state_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ApplicationRecord
     belongs_to :prefecture
     belongs_to :days_to_ship
 
+    belongs_to :user
+
     has_one_attached :image
 
 
