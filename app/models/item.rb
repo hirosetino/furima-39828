ff53923ef83f13f3822146d7880d831043a8ae2c
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-07 17:22:23 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,5 +17,6 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1 }
   validates :prefecture_id, presence: true, numericality: { other_than: 1 }
   validates :days_to_ship_id, presence: true, numericality: { other_than: 1 }
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
