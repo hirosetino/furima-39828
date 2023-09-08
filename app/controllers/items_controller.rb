981fb3cf57ef9e28879965bd874f6e9ebb6b06cc
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-08 10:37:21 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,52 +1,52 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :edit]
-    before_action :move_to_index, only: :edit
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: :edit
 
-    def index
-        @items = Item.all.order('created_at DESC')
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @item = Item.new
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def create
-      @item = Item.new(item_params)
-      if @item.save
-          redirect_to root_path
-      else
-          render :new, status: :unprocessable_entity
-      end
-    end
+  private
 
-    def show
-        @item = Item.find(params[:id])
-    end
+  def item_params
+    params.require(:item).permit(:name, :content, :category_id, :state_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id,
+                                 :price, :image).merge(user_id: current_user.id)
+  end
 
-    def edit
-        @item = Item.find(params[:id])
-    end
+  def move_to_index
+    @item = Item.find(params[:id])
+    return if @item.user.id == current_user.id
 
-    def update
-        @item = Item.find(params[:id])
-        if @item.update(item_params)
-            redirect_to item_path(@item.id)
-        else
-            render :edit, status: :unprocessable_entity
-        end
-    end
-
-    private
-
-    def item_params
-        params.require(:item).permit(:name, :content, :category_id, :state_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id,
-                                  :price, :image).merge(user_id: current_user.id)
-    end
-
-    def move_to_index
-        @item = Item.find(params[:id])
-        unless @item.user.id == current_user.id
-          redirect_to root_path
-        end
-    end
+    redirect_to root_path
+  end
 end
