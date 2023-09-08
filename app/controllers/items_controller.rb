a9bd994e2aa9854c1f7316de1c10375fd05a8ab6
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-08 14:46:44 +0900

    コントローラーアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :find_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :find_item, only: [:show, :edit, :update,:destroy]
   before_action :move_to_index, only: :edit
 
   def index
@@ -34,7 +34,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
