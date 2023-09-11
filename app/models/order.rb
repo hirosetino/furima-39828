ad7df84849a0d3ace52e6d442fb1d141f41a6168
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-11 11:54:10 +0900

    バリデーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
     balongs_to :history
+
+    VALID_POST_CODE_REGEX = /\A\d{3}-\d{4}\z/
+    validates :post_code, precense: true, format: { with: VALID_POST_CODE_REGEX }
+    validates :prefecture_id, precense: true
+    validates :municipalities, precense: true
+    validates :street_address, precense: true
+    validates :telephone_number, precense: true, numericality: { only_integer: true },
+    length: { minimum: 10, maximum: 11 }
 end
