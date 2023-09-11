75dbd47cc7d9549044180d9ecf44b69489efe75b
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-11 12:29:22 +0900

    Formオブジェクトのインスタンスを生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,20 @@
 class OrdersController < ApplicationController
     def index
-    end
-
-    def new
+        @order_history = OrderHistory.new
     end
 
     def create
+        @order_history = order_history.new(order_params)
+        if @order_history.valid?
+        @order_history.save
+        redirect_to root_path
+        else
+        render :index, status: :unprocessable_entity
+        end
     end
 
     private
-    def
+    def order_params
+        params.require(:order_history).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :history.id).merge(user: current_user.id, item: item.id)
     end
 end
