4c3ba57152200f7cf6f31e0bce253f073778adfb
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-11 12:10:25 +0900

    Formオブジェクト　バリデーション移動

---
+++ app/models/order_history.rb
@@ -0,0 +1,17 @@
+class OrderHistory
+    include ActiveModel::Model
+    attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :user, :item
+
+    with_option precense: true do
+        VALID_POST_CODE_REGEX = /\A\d{3}-\d{4}\z/
+        validates :post_code,  format: { with: VALID_POST_CODE_REGEX }
+        validates :prefecture_id, numericality: { other_than: 1 }
+        validates :municipalities
+        validates :street_address
+        validates :telephone_number, numericality: { only_integer: true },
+        length: { minimum: 10, maximum: 11 }
+        validates :history
+        validates :user
+        validates :item
+    end
+  end
