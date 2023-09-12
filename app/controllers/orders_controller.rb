80f19a6f7add322fce42da1d2ff61ada46cab273
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-12 17:03:51 +0900

    Rubocop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,48 +1,50 @@
 class OrdersController < ApplicationController
-    before_action :set_item, only: [:index, :create]
-    before_action :authenticate_user!, only: [:index, :create]
-    before_action :move_to_index, only: [:index, :create]
-    def index
-        gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-        @history_order = HistoryOrder.new
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+  def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @history_order = HistoryOrder.new
+  end
+
+  def create
+    @history_order = HistoryOrder.new(order_params)
+
+    if @history_order.valid?
+      pay_item
+      @history_order.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @history_order = HistoryOrder.new(order_params)
+  private
 
-        if @history_order.valid?
-            pay_item
-            @history_order.save
-            redirect_to root_path
-        else
-            gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-            render :index, status: :unprocessable_entity
-        end
-    end
+  def set_item
+    item_id = params[:item_id]
+    @item = Item.find(item_id)
+  end
 
-    private
+  def order_params
+    params.require(:history_order).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
+  end
 
-    def set_item
-        item_id = params[:item_id]
-        @item = Item.find(item_id)
-    end
+  def move_to_index
+    return unless @item.user_id == current_user.id || @item.history.present?
 
-    def order_params
-        params.require(:history_order).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
-    end
+    redirect_to root_path
+  end
 
-    def move_to_index
-        if @item.user_id == current_user.id || @item.history.present?
-            redirect_to root_path
-        end
-    end
-
-    def pay_item
-        Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-        Payjp::Charge.create(
-            amount: @item.price, 
-            card: order_params[:token], 
-            currency: 'jpy'
-        )
-    end
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- spec/models/history_order_spec.rb
+++ spec/models/history_order_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe HistoryOrder, type: :model do
 
   describe '商品購入機能' do
     context '商品購入できる場合' do
-        it 'post_code, prefecture_id, municipalities, street_address, building_name, telephone_number, user_id, item_id, tokenが存在すれば購入できる' do
-          expect(@history_order).to be_valid
-        end
-        it 'building_nameが無くても購入できる' do
-          @history_order.building_name = ''
-          expect(@history_order).to be_valid
-        end
+      it 'post_code, prefecture_id, municipalities, street_address, building_name, telephone_number, user_id, item_id, tokenが存在すれば購入できる' do
+        expect(@history_order).to be_valid
+      end
+      it 'building_nameが無くても購入できる' do
+        @history_order.building_name = ''
+        expect(@history_order).to be_valid
+      end
     end
 
     context '商品購入できない場合' do
@@ -60,7 +60,7 @@ RSpec.describe HistoryOrder, type: :model do
       it 'post_codeに-がないと購入できない' do
         @history_order.post_code = '1234567'
         @history_order.valid?
-        expect(@history_order.errors.full_messages).to include("Post code is invalid")
+        expect(@history_order.errors.full_messages).to include('Post code is invalid')
       end
       it 'prefecture_idが1だと購入できない' do
         @history_order.prefecture_id = '1'
@@ -70,22 +70,22 @@ RSpec.describe HistoryOrder, type: :model do
       it 'telephone_numberに全角数字が入っていると購入できない' do
         @history_order.telephone_number = '123456789０'
         @history_order.valid?
-        expect(@history_order.errors.full_messages).to include("Telephone number is not a number")
+        expect(@history_order.errors.full_messages).to include('Telephone number is not a number')
       end
       it 'telephone_numberに文字が入っていると購入できない' do
         @history_order.telephone_number = '123456789十'
         @history_order.valid?
-        expect(@history_order.errors.full_messages).to include("Telephone number is not a number")
+        expect(@history_order.errors.full_messages).to include('Telephone number is not a number')
       end
       it 'telephone_numberが9桁以下だと購入できない' do
         @history_order.telephone_number = '123456789'
         @history_order.valid?
-        expect(@history_order.errors.full_messages).to include("Telephone number is too short (minimum is 10 characters)")
+        expect(@history_order.errors.full_messages).to include('Telephone number is too short (minimum is 10 characters)')
       end
       it 'telephone_numberが12桁以上だと購入できない' do
         @history_order.telephone_number = '123456789012'
         @history_order.valid?
-        expect(@history_order.errors.full_messages).to include("Telephone number is too long (maximum is 11 characters)")
+        expect(@history_order.errors.full_messages).to include('Telephone number is too long (maximum is 11 characters)')
       end
     end
   end
